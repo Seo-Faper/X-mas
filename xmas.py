6b214a53ca0546a90f6394e2bbea94f598cffca5
Faper <43310141+Seo-Faper@users.noreply.github.com>
2024-12-24 20:30:51 +0900

    Create xmas.py

---
+++ xmas.py
@@ -0,0 +1,37 @@
+import random
+_                    ='^'
+_                   =1#@@
+_                  = 1//1;
+_                = print("")
+g                    = "\033[32m"
+bg             = "\033[47m"
+b                    = "\033[33m"
+rs               = "\033[0m"
+lc                    = 3#/
+bb                  = 4#@
+ss              = [1, 3, 5]
+tw         = bb + (lc - 1) * (bb // 2)
+for l in range(lc):#0xcaffee$[0]
+    ch              = bb + l; (1//1)
+    i_              = ss[l]#_____\
+    for i in range(ch):# #@________';
+        s_           = " " * (tw - i_ // 2 - i - 1)
+        st_       = "".join(
+        f"{bg if random.choice([True, False]) else ''}{g}*{rs}"
+                for _ in range(i_ + 2 * i)#/@
+                       )#//*\\^
+        print(f"{s_}{st_}")#\\\^
+_               =1//2//int('3'[0])#
+ww              = 3#\$#[1]
+hh                 = 3 #\.@
+for _ in range(hh):
+    s_               = " " * (tw - ww // 2 - 1)
+    t_                = f"{b}|{rs}" * ww
+    1/1;1/1;1/1/1;print(f"{s_}{t_}")#_/@_####
+_               = ( __annotations__)
+_               = __cached__#\?@
+_                 = (__cached__)
+_                =__import__
+_                 =__file__
+_                 =__file__
+#####################################
